3f1d7bd808888d7a79771c4d4c5c5f3ce50526bd
Laggeros <33031839+Laggeros@users.noreply.github.com>
2021-09-15 23:16:19 +0200

    Added 2 libraries

--- humid/humid.ino
+++ humid/humid.ino
@@ -3,12 +3,7 @@ LiquidCrystal_I2C lcd(0x27,16,2);
 #include <Adafruit_BME280.h>
 Adafruit_BME280 bme;
 #include "BME.h"
-#define SensorPin A0 
-
-int sensorValue = 0; 
-const int AirValue = 520; 
-const int WaterValue = 176;
-
+#include "readSoilHumidity.h"
 
 void setup(){
   Serial.begin(9600); 
@@ -30,32 +25,4 @@ void loop() {
   }
   
  delay(30000); 
-}
-
-int readSoilHumidity(){
-  for (int i = 0; i < 10; i++) { 
-    sensorValue = sensorValue + analogRead(SensorPin); 
-    delay(3); 
-  } 
-
- sensorValue = sensorValue/10; 
- Serial.println(sensorValue);
-
- lcd.print("Soil humid:"); 
- lcd.setCursor(11,0);
- lcd.print("    ");
- lcd.setCursor(11,0);
- lcd.print(convertToPercent(sensorValue)); 
- lcd.print("%");
- 
- sensorValue = 0;
-}
-
-int convertToPercent(int value){
-  int percentValue = 0;
-  percentValue = map(value, AirValue, WaterValue, 0, 100);
-  if (percentValue > 100) percentValue = 100;
-  if (percentValue < 0) percentValue = 0;
-  value = 0;
-  return percentValue;
 }
