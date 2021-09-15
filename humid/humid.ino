76be6c02828ea9282678e29f88ff7ac435f42b40
Laggeros <33031839+Laggeros@users.noreply.github.com>
2021-09-15 23:08:24 +0200

    Added BME file

--- humid/humid.ino
+++ humid/humid.ino
@@ -1,9 +1,9 @@
-#include <Adafruit_BME280.h>
 #include <LiquidCrystal_I2C.h>
-#define SensorPin A0 
-Adafruit_BME280 bme;
-
 LiquidCrystal_I2C lcd(0x27,16,2);
+#include <Adafruit_BME280.h>
+Adafruit_BME280 bme;
+#include "BME.h"
+#define SensorPin A0 
 
 int sensorValue = 0; 
 const int AirValue = 520; 
@@ -29,7 +29,7 @@ void loop() {
    BME();
   }
   
- delay(60000); 
+ delay(30000); 
 }
 
 int readSoilHumidity(){
@@ -58,26 +58,4 @@ int convertToPercent(int value){
   if (percentValue < 0) percentValue = 0;
   value = 0;
   return percentValue;
-}
-
-int BME(){
-  Serial.print(F("Temperature = "));
-    Serial.print(bme.readTemperature());
-    Serial.println(" *C");
-    lcd.setCursor(0,1);lcd.print("T:");lcd.print(bme.readTemperature()); lcd.print(" ");
-
-    Serial.print(F("Pressure = "));
-    Serial.print(bme.readPressure()/100);
-    Serial.println(" hPa");
-
-    Serial.print(F("Approx altitude = "));
-    Serial.print(bme.readAltitude(1019.66)); 
-    Serial.println(" m");
-
-    Serial.print(F("Humidity = "));
-    Serial.print(bme.readHumidity());
-    Serial.println(" %");
-    lcd.print("H:"); lcd.print(bme.readHumidity()); lcd.print("%"); 
-
-    Serial.println();
 }
